3a147510d6541e95fa31f68e2a4f3d22a84f8807
Hejiahao <14858979+hzzz-lx@user.noreply.gitee.com>
2024-10-10 19:06:02 +0800

    3

--- src/views/CanKu.vue
+++ src/views/CanKu.vue
@@ -11,9 +11,9 @@
       </div>
       <div class="mt-5">所属项目</div>
       <a-input-group compact class="mt-3">
-        <a-select v-model:value="value17" style="width: 30%">
-          <a-select-option value="Home">Js</a-select-option>
-          <a-select-option value="Company">C++</a-select-option>
+        <a-select v-model:value="value4" style="width: 30%">
+          <a-select-option value="Js">Js</a-select-option>
+          <a-select-option value="C++">C++</a-select-option>
         </a-select>
       </a-input-group>
       <div class="flex flex-row mt-5">
@@ -47,7 +47,7 @@
         >添加.gitignore文件</a-checkbox
       >
       <div class="mt-5">是否开源</div>
-      <a-radio-group  v-model:value="value" name="radioGroup">
+      <a-radio-group v-model:value="value" name="radioGroup">
         <a-radio class="mt-3" value="1"
           >私有仓库(仅对仓库成员可见, 仓库成员可访问仓库)</a-radio
         >
@@ -55,9 +55,17 @@
         <a-radio class="mt-3" value="2">公开仓库</a-radio>
       </a-radio-group>
       <div class="mt-5">
-          <button @click="btn" class=" bg-black w-28 h-7 rounded mr-5 text-white">完成创建</button>
-           <button @click="btn1" class=" w-20 h-7 rounded text-black" style="border: 1px gray solid;">取消</button>
-        </div>
+        <button @click="btn" class="bg-black w-28 h-7 rounded mr-5 text-white">
+          完成创建
+        </button>
+        <button
+          @click="btn1"
+          class="w-20 h-7 rounded text-black"
+          style="border: 1px gray solid"
+        >
+          取消
+        </button>
+      </div>
     </div>
   </a-layout-content>
 </template>
@@ -65,20 +73,49 @@
 import { Icon } from "@iconify/vue/dist/iconify.js";
 import { useRouter } from "vue-router";
 import { ref } from "vue";
-const value = ref('1');
+import { message } from "ant-design-vue";
+
+const value = ref("1");
 const value1 = ref("Git仓库");
 const value2 = ref("");
 const value3 = ref("");
+const value4 = ref("");
 const checked = ref(false);
 const checked1 = ref(false);
 const router = useRouter();
+
 const back = () => {
   router.replace("/");
 };
-const btn = ()=>{
-    router.replace('/home1')
-}
-const btn1 = ()=>{
-    router.replace('/')
-}
+
+const btn = () => {
+  // 检查必填项
+  if (!value1.value.trim()) {
+    message.warning("仓库类型未填写");
+    return;
+  }
+  if (!value2.value.trim()) {
+    message.warning("仓库名称未填写");
+    return;
+  }
+  if (!value3.value.trim()) {
+    message.warning("仓库描述未填写");
+    return;
+  }
+  if (!value4.value.trim()) {
+    message.warning("所属项目未填写");
+    return;
+  }
+  if (checked.value != true && checked1.value != true) {
+    message.warning("初始化仓库未填写");
+    return;
+  }
+
+  // 如果所有必填项都已填写，进行跳转
+  router.replace("/home1");
+};
+
+const btn1 = () => {
+  router.replace("/");
+};
 </script>
